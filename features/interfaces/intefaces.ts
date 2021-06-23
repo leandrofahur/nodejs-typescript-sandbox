8ce69424b2f42d242f777050b280d297976b2f14
Leandro <fahurleandro@gmail.com>
2021-06-23 10:48:31 -0700

    flexibility

--- features/interfaces/intefaces.ts
+++ features/interfaces/intefaces.ts
@@ -1,20 +1,31 @@
-interface Vehicle {
-  name: string;
-  year: number;
-  broken: boolean;
+interface Reportable {
+  summary(): string;
 }
-
-const printVehicle = ({ name, year, broken }: Vehicle): void => {
-  console.log(`
-    Vehicle name ${name}
-    From the year ${year}
-    Is it broken? ${broken}`);
-};
 
 const oldCivic = {
   name: "civic",
   year: 2000,
   broken: true,
+  summary(): string {
+    return `
+    Vehicle name ${this.name}
+    From the year ${this.year}
+    Is it broken? ${this.broken}\n`;
+  },
 };
 
-printVehicle(oldCivic);
+const drink = {
+  color: "brown",
+  carbonated: true,
+  sugar: 40,
+  summary(): string {
+    return `My drink has ${this.sugar} grams of sugar`;
+  },
+};
+
+const printSummary = (item: Reportable): void => {
+  console.log(item.summary());
+};
+
+printSummary(oldCivic);
+printSummary(drink);
